e16ef7fc6388f908200a98a28cd35de5a4a76bad
Akshay Panpaliya <148350221+AkshayPanpaliya@users.noreply.github.com>
2026-07-14 23:05:07 +0530

    Time: 2 ms (71.03%) | Memory: 44.8 MB (45.18%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,27 +1,24 @@
 class Solution {
     public void check(int n,List<String>list,String curr,int open,int close)
     {
-        if(curr.length()==2*n)
-        {
-            list.add(curr);
-            return;
-        }
-        if(open<n)
-        {
-            check(n,list,curr+"(",open+1,close);
-        }
-        if(close<open)
-        {
-            
-            check(n,list,curr+")",open,close+1);
-        }
+       if(curr.length()==2*n)
+       {
+        list.add(curr);
+        return ;
+       }
+       if(open<n)
+       {
+        check(n,list,curr+"(",open+1, close);
+       }
+       if(close<open)
+       {
+        check(n,list,curr+ ")",open, close+1);
+       }
     }
-    public List<String> generateParenthesis(int n) {
+    public List<String> generateParenthesis(int n)
+    {
         List<String> list=new ArrayList<>();
-        int left=0;
-        int right=0;
-        String curr="";
-        check(n,list,curr,left,right);
+        check(n,list,"",0,0);
         return list;
     }
 }
